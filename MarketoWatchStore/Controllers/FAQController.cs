507e75616277c068619854bc5e07a53d10ca6333
kamilquliyev98 <kamilag@code.edu.az>
2022-06-18 20:20:24 +0400

    added faq controller with view

---
+++ MarketoWatchStore/Controllers/FAQController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketoWatchStore.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
