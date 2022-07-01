510107bc4e280ef09f233b71caf1b48b58dbe4ee
kamilquliyev98 <kamilag@code.edu.az>
2022-07-01 20:54:58 +0400

    Shopping Cart Edits

--- MarketoWatchStore/Controllers/CompareListController.cs
+++ MarketoWatchStore/Controllers/CompareListController.cs
@@ -1,5 +1,9 @@
 ﻿using MarketoWatchStore.DAL;
+using MarketoWatchStore.Models;
+using MarketoWatchStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +19,29 @@ namespace MarketoWatchStore.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            string cookieCompare = HttpContext.Request.Cookies["compare"];
+
+            List<CompareListVM> compareListVMs = new List<CompareListVM>();
+
+            if (!string.IsNullOrEmpty(cookieCompare))
+            {
+                compareListVMs = JsonConvert.DeserializeObject<List<CompareListVM>>(cookieCompare);
+
+                foreach (CompareListVM compareListVM in compareListVMs)
+                {
+                    Product dbProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == compareListVM.ProductId);
+
+                    compareListVM.Title = dbProduct.Title;
+                    compareListVM.MainImage = dbProduct.MainImage;
+                    compareListVM.Price = (dbProduct.DiscountPrice != null && dbProduct.DiscountPrice > 0) ? (double)dbProduct.DiscountPrice : dbProduct.Price;
+                    compareListVM.Gender = dbProduct.Gender;
+                    //compareListVM.Features = dbProduct.FeatureIds
+                }
+            }
+
+            return View(compareListVMs);
         }
     }
 }
--- MarketoWatchStore/ViewModels/CompareListVM.cs
+++ MarketoWatchStore/ViewModels/CompareListVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MarketoWatchStore.Enums;
+using MarketoWatchStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +14,12 @@ namespace MarketoWatchStore.ViewModels
         public string Title { get; set; }
         public string MainImage { get; set; }
         public double Price { get; set; }
+        public GenderType Gender { get; set; }
+        public string Brand { get; set; }
+        public string Display { get; set; }
+        public string PowerSource { get; set; }
+        public string SpecialType { get; set; }
+        public IEnumerable<Feature> Features { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
     }
 }
