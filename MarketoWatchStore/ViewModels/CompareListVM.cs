7bf3817f197b308ba5e70f364e002c10359d8a1d
kamilquliyev98 <kamilag@code.edu.az>
2022-07-01 19:32:50 +0400

    Shopping Cart Fixed_v2

---
+++ MarketoWatchStore/ViewModels/CompareListVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketoWatchStore.ViewModels
+{
+    public class CompareListVM
+    {
+        public int ProductId { get; set; }
+
+        public string Title { get; set; }
+        public string MainImage { get; set; }
+        public double Price { get; set; }
+    }
+}
