e08402c24dd1051ce94d82fdeb768464f7344d64
kamilquliyev98 <kamilag@code.edu.az>
2022-07-10 14:25:52 +0400

    search form submit btn

--- MarketoWatchStore/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/assets/js/script.js
+++ MarketoWatchStore/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/assets/js/script.js
@@ -1,5 +1,10 @@
 /* Document ready functions */
 $(document).ready(function () {
+    /* Feather JS */
+    feather.replace();
+
+
+
     /* Dark/Light mode JS */
     let moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
@@ -50,11 +55,6 @@ $(document).ready(function () {
             localStorage.setItem("dark_mode", "1");
         }
     });
-
-
-
-    /* Feather JS */
-    feather.replace();
 
 
 
@@ -529,6 +529,13 @@ $(document).ready(function () {
             $qty.val(currentVal - 1);
             changeCartProductCount($qty.val());
         }
+    });
+
+
+
+    /* Seacrh button type submit */
+    $(document).on("click", "#searchBtn", function () {
+        $("#searchForm").submit();
     });
 
 
